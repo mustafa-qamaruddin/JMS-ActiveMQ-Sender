901bc247e1c3a918131a215e926fc4c23c2718b9
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-07-19 15:13:14 +0200

    jms sender

--- src/main/java/com/apress/messaging/SenderApplication.java
+++ src/main/java/com/apress/messaging/SenderApplication.java
@@ -1,7 +1,11 @@
 package com.apress.messaging;
 
+import com.apress.messaging.config.JMSProperties;
+import com.apress.messaging.jms.SimpleSender;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SenderApplication {
@@ -10,4 +14,10 @@ public class SenderApplication {
 		SpringApplication.run(SenderApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner simple(JMSProperties props, SimpleSender sender) {
+		return args -> {
+			sender.sendMessage(props.getQueue(), "Hello, World!");
+		};
+	}
 }
